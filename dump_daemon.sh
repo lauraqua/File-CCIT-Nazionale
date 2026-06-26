5370693495228f73a5d5212a461adbd264be79ff
lauraqua <113291271+lauraqua@users.noreply.github.com>
2026-06-26 12:18:31 +0200

    Add files via upload

---
+++ dump_daemon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do 
+	rsync -avz "vulnbox:/root/pcaps/round_[0-9]*" ./ # sincronizza la vulnbox alla macchina host, in particolare il primo path della vb a questa cartella
+	find . -mindepth 1 -maxdepth 1 -name "round_[0-9]*" -exec ./upload_and_rename.sh '{}' \; #trova tutti i file con il nome round... e li carica ad upload....sh
+	sleep 60  #tutto ciò ogni 30 sec
+done
