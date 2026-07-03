ddcde960f27a5364aeda7323808b5401372c186a
lauraqua <113291271+lauraqua@users.noreply.github.com>
2026-07-03 17:19:33 +0200

    Update dump_daemon.sh

--- dump_daemon.sh
+++ dump_daemon.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 while true; do 
-	rsync -avz "../VulnBox/"round*.gz ./ # sincronizza la vulnbox alla macchina host, in particolare il primo path della vb a questa cartella
+	rsync -avz "vulnbox:/root/pcaps/"round*.gz ./ # sincronizza la vulnbox alla macchina host, in particolare il primo path della vb a questa cartella
 
-	#"vulnbox:/root/pcaps/round_[0-9]*"
-	
+	#"vulnbox:/root/pcaps/"round*.gz
+	# "../VulnBox/"round*.gz
+
 	find . -mindepth 1 -maxdepth 1 -name "round*.gz" -exec ./upload_and_rename.sh '{}' \; #trova tutti i file con il nome round... e li carica ad upload....sh
 	sleep 30  #tutto ciò ogni 30 sec
 done
